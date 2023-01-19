10b407f0b06350199a816ccd8e190ad0d71019a8
fceja <88167774+fceja@users.noreply.github.com>
2023-01-19 14:11:22 -0800

    base

---
+++ mysite/main/urls.py
@@ -0,0 +1,12 @@
+
+# from django import path
+from django.urls import path
+# dot references current directory
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('sign-up', views.sign_up, name='sign_up')
+
+]
