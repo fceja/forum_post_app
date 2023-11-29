88069fbdd928c67a6265bf7cf0b5925bb66dd8d3
fceja <cisco_ceja@icloud.com>
2023-11-28 20:44:26 -0800

    fix heroku issue attempt

--- django_site/settings.py
+++ django_site/settings.py
@@ -128,6 +128,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'main_site/static'),
 )
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
