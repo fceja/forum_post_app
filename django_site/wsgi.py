24d780f889d0a65b4cd7cc2541a4d9de9da22d7d
fceja <cisco_ceja@icloud.com>
2023-11-28 17:07:16 -0800

    rename dir

--- django_site/wsgi.py
+++ django_site/wsgi.py
@@ -16,6 +16,6 @@ path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_site.settings')
 
 application = get_wsgi_application()
