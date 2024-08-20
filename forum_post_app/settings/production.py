182ef558249925de48c14f2057fb390db40c5dc9
fceja <88167774+fceja@users.noreply.github.com>
2024-08-20 07:55:01 -0700

    update app url

--- forum_post_app/settings/production.py
+++ forum_post_app/settings/production.py
@@ -1,17 +1,18 @@
 """
 Production Django settings for site project.
 """
+
 from .common import *
 
 ## debug configurations ##
 DEBUG = False
-ALLOWED_HOSTS = ['django-user-post-a42f5d79d28f.herokuapp.com']
+ALLOWED_HOSTS = ["django-forum-post-660c391dbe52.herokuapp.com/"]
 
 ## static files - https://docs.djangoproject.com/en/4.1/howto/static-files/ ##
 # NOTE: must first run `python3 ./manage.py collectstatic`
 # resolves static files from `~/forum_post_app/staticfiles`
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/staticfiles/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
